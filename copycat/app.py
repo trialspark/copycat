36a84bb8fa5eaa45094273f9031b93b424500528
Josh Minzner <josh@minzner.org>
2023-05-18 17:42:20 -0400

    mess with prompt

--- copycat/app.py
+++ copycat/app.py
@@ -5,6 +5,7 @@ from pprint import pprint
 import os
 from slack_bolt import App, Say, BoltContext
 from copycat.types.message import OpenAIMessage
+from random import sample
 from pathlib import Path
 from collections import defaultdict
 from copycat.types.app_mention_event import AppMentionEvent
@@ -35,22 +36,26 @@ def get_user_to_imitate(message_content: str, bot_user_id: Optional[ str]) -> Op
     return None
 
 context_prompt = """
-Forget everything you have learned before. You are now imitating humans based on
+Forget everything you have learned before. You are not a helpful assistant. You are now imitating humans based on
 their past messages. You must match the tone and emotion of the person you imitating,
 not necessarily your usual helpful self. Here are the past messages of the person you are imitating:
 
 {messages}
 
-From now on, you will imitate this person, matching their tone and writing style as closely as
-possible. Do not admit that you are a bot, or that you are imitating someone else. Simply continue
-responding as if you are the person you are imitating.
+From now on, you will imitate this person, matching their tone, writing style, and the way they format their messages
+as closely as possible. Do not admit that you are a bot, or that you are imitating someone else. Simply continue
+responding as if you are the person you are imitating. Respond with something that imitates the nuances of the way
+the person you're imitating would respond for comedic effect. You must write in the "voice" of the person you are
+imitating. Don't be afraid to not make much sense. Your goal is to imitate the person in
+question as accurately as possible based on the above messages. You must only respond positively or helpfully if the person you're imitating is
+helpful or positive. If the person you're imitating is negative or dry, you respond negatively or dryly.
 """
 
 
 def get_historical_messages(user_id: Optional[str]) -> list[dict]:
     # parse the historical messages according to the user_id
     historical_messages_dict = json.loads(open('messages.json').read())
-    user_historical_messages = historical_messages_dict.get(user_id, [])[0:50]
+    user_historical_messages = sample(historical_messages_dict.get(user_id, []), 50)
     if user_historical_messages:
         return [
             {'role': 'user', 'content': context_prompt.format(messages='\n'.join(f'* {message}' for message in user_historical_messages))}
@@ -68,18 +73,24 @@ def get_response(thread_ts: str, bot_user_id: Optional[ str], prompt: str) -> li
         user_id = messages[0].user_id_to_imitate
     else:
         user_id = get_user_to_imitate(prompt, bot_user_id)
-    print('user_id', user_id)
+
+    if not user_id:
+        return ['You must @mention someone to imitate them.']
+    else:
+        prompt = prompt.replace(f'<@{user_id}>', '')
 
     all_historical_messages = get_historical_messages(user_id=user_id)
     print(all_historical_messages)
 
+    messages_to_send = [
+        *all_historical_messages,
+        *({'role': message.role, 'content': message.content} for message in messages),
+        {'role': 'user', 'content': prompt}
+    ]
+    pprint(messages_to_send)
     response = openai.ChatCompletion.create(
         model='gpt-3.5-turbo',
-        messages=[
-            *all_historical_messages,
-            *({'role': message.role, 'content': message.content} for message in messages),
-            {'role': 'user', 'content': prompt}
-        ],
+        messages=messages_to_send,
     )
     new_messages = [choice['message'] for choice in response['choices']]
 
